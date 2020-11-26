78f02500df90fc85ca2dcc472b376c61d822bae7
denikond <74867259+denikond@users.noreply.github.com>
2020-11-26 23:53:16 +0300

    Update LP19_lesson_if1.py

--- to_check/11 lesson_if1/LP19_lesson_if1.py
+++ to_check/11 lesson_if1/LP19_lesson_if1.py
@@ -13,21 +13,21 @@ age_work_stop = 65
 
 def what_can_i_do(age):
     you_do = []
-    if (age<age_playsch_start):
+    if age < age_playsch_start:
         you_do.append("Лучше быть с мамой дома")
-    if (age>=age_playsch_start) and (age<=age_playsch_stop):
+    if age >= age_playsch_start and age <= age_playsch_stop:
         you_do.append("Можно быть в садике")
-    if (age>=age_school_start) and (age<=age_school_stop):
+    if age >= age_school_start and age <= age_school_stop:
         you_do.append("Можно быть в школе")
-    if (age>=age_hi_school_start) and (age<=age_hi_school_stop):
+    if age >= age_hi_school_start and age <= age_hi_school_stop:
         you_do.append("Можно быть в ВУЗе")
-    if (age>=age_work_start) and (age<=age_work_stop):
+    if age >= age_work_start and age<=age_work_stop:
         you_do.append("Можно работать")
-    if (age>age_work_stop):
+    if age > age_work_stop:
         you_do.append("Лучше отдохнуть")
     return you_do
 
-age=int(input("Введите возраст "))
+age = int(input("Введите возраст "))
 ido = what_can_i_do(age)
 for i in range(len(ido)):
     print(ido[i])
